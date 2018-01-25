96fa7173e754fee856d237052ad9e9012b96011d
Rémy Melquiond <remy@XPS_Remy.localdomain>
2018-01-25 15:04:46 +0100

    WIP add weather api call

--- node/REST.js
+++ node/REST.js
@@ -1,32 +1,10 @@
 var bcrypt = require('bcrypt');
 var request = require('request-promise');
 
-var change = {};
-
 function REST_ROUTER(router, change) {
     var self = this;
-    self.getCurrenciesChange();
     self.handleRoutes(router);
 }
-
-REST_ROUTER.prototype.getChange = function()
-{
-  return change;
-}
-
-REST_ROUTER.prototype.setChange = function(value)
-{
-  change = value;
-}
-
-REST_ROUTER.prototype.getCurrenciesChange = function()
-{
-  var self = this;
-  request("https://openexchangerates.org/api/latest.json?app_id=5063db9510d0407c9a823394f3b4487d", function(error, response, body) {
-    console.log("response: " + body + " error: " + error);
-    self.setChange(JSON.parse(body));
-  });
-};
 
 REST_ROUTER.prototype.convert = function(currency, value, targetCurrency)
 {
@@ -50,14 +28,12 @@ REST_ROUTER.prototype.convert = function(currency, value, targetCurrency)
 
 REST_ROUTER.prototype.handleRoutes = function(router) {
     var self = this;
-    router.get("/currencies", function(req,res){
-      if(self.getChange() !== undefined){
-        var result = self.getChange();
-        result = result.rates;
-        res.json({"Error" : false, "rates" : Object.keys(result)});
-      }else{s
-        res.json({"Error" : true, "Message" : "Change could not be retrieved"});
-      }
+    router.get("/weather/:lat/:lon", function(req,res){
+      var self = this;
+      request("http://api.openweathermap.org/data/2.5/weather?lat=" + req.params.lat + "&lon=" + req.params.lon + "&mode=html&APPID=9341dfa7fc06f5af2fb53fac204cfd8b", function(error, response, body) {
+        console.log("response: " + body + " error: " + error);
+        res.json({"error" : false, "body" : body});
+      });
     });
     router.get("/convert/:currency1/:value1/:currency2", function(req,res){
         var data = {
